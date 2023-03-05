b909ea26a6e598651e1a0770c2917dd33c831869
5trange <adithmurali@protonmail.com>
2023-03-05 13:13:00 +0530

    Add a basic skeleton app

---
+++ app/src/main/java/com/strange/octoplay/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.strange.octoplay
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity  : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        setupWithNavController(bottomNavigationView, navController)
+    }
+}
